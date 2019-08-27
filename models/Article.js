0bf66b423e823afc66551f7904d336528953fef2
Caleb Myers <cmyers5108@gmail.com>
2019-08-26 22:06:47 -0400

    updated layout and added buttons to scrape and clear data, updated comment board

--- models/Article.js
+++ models/Article.js
@@ -16,7 +16,7 @@ var ArticleSchema = new Schema({
     required: true,
     unique: true
   },
-  comment: [
+  comments: [
     {
       type: Schema.Types.ObjectId,
       ref: "Comment"
--- server.js
+++ server.js
@@ -15,27 +15,27 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoNews";
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   db.Article.find({})
-    .then(function(articles) {
+    .then(function (articles) {
       res.render("index", { articles: articles });
     })
-    .catch(function(err) {
+    .catch(function (err) {
       res.json(err);
     });
 });
 
-app.get("/scrape", function(req, res) {
-  axios.get("http://www.mtv.com/news/music/").then(function(response) {
+app.get("/scrape", function (req, res) {
+  axios.get("http://www.mtv.com/news/music/").then(function (response) {
     var $ = cheerio.load(response.data);
 
-    $("header.post-header").each(function(i, element) {
+    $("header.post-header").each(function (i, element) {
       var result = {};
 
       result.title = $(this).children("h1.headline").children().children().text().trim();
@@ -43,16 +43,59 @@ app.get("/scrape", function(req, res) {
       result.link = $(this).children("h1.headline").children().attr("href");
 
       db.Article.create(result)
-        .then(function(newArticle) {
+        .then(function (newArticle) {
           console.log(newArticle);
         })
-        .catch(function(err) {
+        .catch(function (err) {
           console.log(err);
         })
     })
-
-    res.send("Scrape Complete");
+    db.Article.find({})
+      .then(function (articles) {
+        res.render("index", { articles: articles });
+      })
+      .catch(function (err) {
+        res.json(err);
+      });
   })
+});
+
+app.get("/clear", function (req, res) {
+  db.Article.deleteMany({})
+    .then(function (data) {
+      res.render("index", { articles: articles });
+    })
+    .catch(function (err) {
+      res.json(err)
+    })
+})
+
+app.get("/articles/:id", function (req, res) {
+  db.Article.findOne({ _id: req.params.id })
+    .populate("comments")
+    .then(function (article) {
+      res.json(article);
+    })
+    .catch(function (err) {
+      res.json(err);
+    });
+});
+
+app.post("/articles/:id", function (req, res) {
+  db.Comment.create(req.body)
+    .then(function (newComment) {
+      return db.Article.findOneAndUpdate({ _id: req.params.id }, { $push: { comments: newComment._id } }, { new: true })
+    })
+    .then(function (article) {
+      res.json(article)
+    })
+    .catch(function (err) {
+      res.json(err)
+    })
+})
+
+app.get("*", function (req, res) {
+  res.sendStatus(404)
 })
 
 app.listen(PORT, function () {
